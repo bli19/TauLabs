1bd69e2b239836418d9b3f3b76a2967299cc7963
guilhermito <guilhermito.santiago@gmail.com>
2013-12-12 23:33:05 +0000

    GCS:Removed unused variables and typedef

--- ground/gcs/src/plugins/setupwizard/vehicleconfigurationhelper.cpp
+++ ground/gcs/src/plugins/setupwizard/vehicleconfigurationhelper.cpp
@@ -617,7 +617,6 @@ GUIConfigDataUnion VehicleConfigurationHelper::getGUIConfigData()
 {
     GUIConfigDataUnion configData;
 
-    SystemSettings *systemSettings = SystemSettings::GetInstance(m_uavoManager);
     Q_ASSERT(systemSettings);
 
     for (int i = 0; i < (int)(SystemSettings::AIRFRAMECATEGORYSPECIFICCONFIGURATION_NUMELEM); i++) {
