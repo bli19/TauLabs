a35a9ed784bc77502c434499884aed18c58cfc12
bli19 <boli719@gmail.com>
2014-08-22 03:53:35 +0800

    try add stm32f407vet6 device
    try add stm32f407vet6 device

---
+++ flight/targets/tpbrd/target-defs.mk
@@ -0,0 +1,10 @@
+# Add this board to the list of buildable boards
+ALL_BOARDS += tpbrd
+
+# Set the cpu architecture here that matches your STM32
+# Should be one of: f1,f3,f4
+tpbrd_cpuarch := f4
+
+# Short name of this board (used to display board name in parallel builds)
+# Should be exactly 4 characters long.
+tpbrd_short := 'tpbr'
